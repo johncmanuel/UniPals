056fd343b68503e99928d14004322e6917d19363
John Carlo Manuel <jmanuel51801@gmail.com>
2021-03-22 15:55:21 -0700

    using free, open source components for tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
+import Main from "components/Main";
+import Navbar from "components/Navbar";
+import Login from "components/Login";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Login></Login>
+      <Navbar></Navbar>
+      <Main></Main>
+    </div>
+  );
 }
 
 export default App;
