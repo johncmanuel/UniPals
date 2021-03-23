cf3a061b0580d0963360ee0226b155b27c2d5eb4
john-manuel <jmanuel51801@gmail.com>
2021-03-23 15:42:40 -0700

    added friend card for feed

---
+++ frontend/src/components/FriendCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card } from "semantic-ui-react";
+
+const FriendCard = () => (
+  <Card
+    fluid
+    link
+    header="Rick Sanchez"
+    meta="Scientist"
+    description={[
+      "Rick is a genius scientist whose alcoholism and reckless,",
+      " nihilistic behavior are a source of concern for his family.",
+    ].join("")}
+  />
+);
+
+export default FriendCard;
