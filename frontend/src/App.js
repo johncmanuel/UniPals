dd039b31e02bc45bc89a83a43331a55aadca3f79
john-manuel <jmanuel51801@gmail.com>
2021-03-24 14:02:15 -0700

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import Main from "components/Main";
+// import Login from "components/Login";
 import Navbar from "components/Navbar";
-import Login from "components/Login";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import Profile from "components/Profile";
+import Groups from "components/Groups";
+import Notifications from "components/Notifications";
+import Messages from "components/Messages";
 
 function App() {
   return (
-    <div>
-      <Login></Login>
-      <Navbar></Navbar>
-      <Main></Main>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        {/* Keep most specifc paths at the top */}
+        <Route path="/profile" component={Profile} />
+        <Route path="/notifications" component={Notifications} />
+        <Route path="/groups" component={Groups} />
+        <Route path="/messages" component={Messages} />
+
+        <Route path="/" exact component={Main} />
+      </Switch>
+    </Router>
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,6 +4,7 @@ import "dna-responsive-nav/dist/dna-rn.css";
 import { NAMES } from "../names.json";
 import { Icon } from "semantic-ui-react";
 import { isMobile } from "react-device-detect";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const linkCSS = {
   paddingLeft: "10px",
@@ -11,54 +12,68 @@ const linkCSS = {
 
 const links = (
   <ul>
-    <li>
-      <a className="" href="/" aria-label="profile" title="Profile">
+    <li key="user">
+      <Link to="/profile" aria-label="profile" title="Profile">
         <Icon name="user"></Icon>
-      </a>
+      </Link>
     </li>
-    <li>
-      <a className="" href="/" aria-label="groups" title="Groups">
+    <li key="groups">
+      <Link to="/groups" aria-label="groups" title="Groups">
         <Icon name="group"></Icon>
-      </a>
+      </Link>
     </li>
-    <li>
-      <a className="" href="/" aria-label="messages" title="Messages">
+    <li key="messages">
+      <Link to="/messages" aria-label="messages" title="Messages">
         <Icon name="chat"></Icon>
-      </a>
+      </Link>
     </li>
-    <li>
-      <a className="" href="/" aria-label="notifications" title="Notifications">
+    <li key="notifications">
+      <Link
+        to="/notifications"
+        aria-label="notifications"
+        title="Notifications"
+      >
         <Icon name="bell"></Icon>
-      </a>
+      </Link>
+    </li>
+    <li key="signout">
+      <a style={{ cursor: "pointer" }}>Sign Out</a>
     </li>
   </ul>
 );
 
 const linksMobile = (
   <ul>
-    <li>
-      <a className="" href="/" aria-label="profile" title="Profile">
+    <li key="user">
+      <Link to="/profile" aria-label="profile" title="Profile">
         <Icon name="user"></Icon>
         <span style={linkCSS}>User</span>
-      </a>
+      </Link>
     </li>
-    <li>
-      <a className="" href="/" aria-label="groups" title="Group">
+    <li key="groups">
+      <Link to="/groups" aria-label="groups" title="Groups">
         <Icon name="group"></Icon>
         <span style={linkCSS}>Groups</span>
-      </a>
+      </Link>
     </li>
-    <li>
-      <a className="" href="/" aria-label="messages" title="Messages">
+    <li key="messages">
+      <Link to="/messages" aria-label="messages" title="Messages">
         <Icon name="chat"></Icon>
         <span style={linkCSS}>Messages</span>
-      </a>
+      </Link>
     </li>
-    <li>
-      <a className="" href="/" aria-label="notifications" title="Notifications">
+    <li key="notifications">
+      <Link
+        to="/notifications"
+        aria-label="notifications"
+        title="Notifications"
+      >
         <Icon name="bell"></Icon>
         <span style={linkCSS}>Notifications</span>
-      </a>
+      </Link>
+    </li>
+    <li key="signout">
+      <a style={{ cursor: "pointer" }}>Sign Out</a>
     </li>
   </ul>
 );
