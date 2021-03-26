df06187d790364083a80be56d5c3c6e44a553178
john-manuel <jmanuel51801@gmail.com>
2021-03-26 12:47:43 -0700

    refactor friend card

--- frontend/src/components/FriendCard.js
+++ frontend/src/components/FriendCard.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { Card } from "semantic-ui-react";
+import { Card, Image } from "semantic-ui-react";
 
-const FriendCard = (props) => (
-  <Card
-    fluid
-    link
-    header={props.header}
-    meta={props.meta}
-    description={props.description}
-  />
-);
+function FriendCard(props) {
+  const header = props.name + " " + props.surname;
+  const avatarUrl = `https://ui-avatars.com/api/?background=random&name=${props.name}+${props.surname}`;
+  if (props.src === undefined) {
+    return (
+      <Card fluid link>
+        <Card.Content>
+          <Image floated="right" size="mini" src={avatarUrl} rounded />
+          <Card.Header>{header}</Card.Header>
+          <Card.Meta>{props.meta}</Card.Meta>
+          <Card.Description>{props.description}</Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+  return (
+    <Card fluid link>
+      <Card.Content>
+        <Image src={props.src} floated="right" size="mini" rounded />
+        <Card.Header>{header}</Card.Header>
+        <Card.Meta>{props.meta}</Card.Meta>
+        <Card.Description>{props.description}</Card.Description>
+      </Card.Content>
+    </Card>
+  );
+}
 
 export default FriendCard;
