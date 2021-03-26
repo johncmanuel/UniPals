cc48e62454bf54fbbafde89f21ba252e81bc9289
john-manuel <jmanuel51801@gmail.com>
2021-03-26 12:48:49 -0700

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
-import Main from "components/Main";
-import Login from "components/Login";
+import Main from "components/Feed/Main";
+import Login from "components/Auth/Login";
 import Navbar from "components/Navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Profile from "components/Profile";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Profile from "components/User/Profile";
 import Groups from "components/Groups";
 import Notifications from "components/Notifications";
 import Messages from "components/Messages";
-import CharacterForm from "components/CharacterForm";
+import CharacterForm from "components/Forms/CharacterForm";
 import Footer from "components/Footer";
+import SignUp from "components/Auth/SignUp";
 
 function App() {
+  const loggedIn = true;
   return (
     <>
       <Router>
@@ -21,19 +28,28 @@ function App() {
           }}
         >
           <Navbar />
-          <div style={{ flex: 1, overflow: "auto" }}>
-            <Switch>
-              {/* Keep most specifc paths at the top */}
-              <Route path="/profile" component={Profile} />
-              <Route path="/notifications" component={Notifications} />
-              <Route path="/groups" component={Groups} />
-              <Route path="/messages" component={Messages} />
-              <Route path="/characterForm" component={CharacterForm} />
-              <Route path="/login" component={Login} />
+          <Switch>
+            {/* Keep most specifc paths at the top */}
+            {/* Routing model:
+                                                -> messages
+                                                -> profile
+                login -> characterForm -> feed -> notifications
+                                                -> groups
+                                                -> settings
+              */}
 
-              <Route path="/" exact component={Main} />
-            </Switch>
-          </div>
+            <Route path="/profile" component={Profile} />
+            <Route path="/notifications" component={Notifications} />
+            <Route path="/groups" component={Groups} />
+            <Route path="/messages" component={Messages} />
+            <Route path="/characterForm" component={CharacterForm} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/feed" component={Main} />
+
+            <Route path="/" exact>
+              {loggedIn ? <Redirect to="/feed" /> : <Login />}
+            </Route>
+          </Switch>
           <Footer />
         </div>
       </Router>
