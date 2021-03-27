6c2ec0683b591a8a632f95f9ee09e78305b12e39
john-manuel <jmanuel51801@gmail.com>
2021-03-26 19:45:41 -0700

    updated personality test

---
+++ frontend/src/components/Forms/MBTIScoring.js
@@ -0,0 +1,413 @@
+const MBTIScoring = {
+  "3A": {
+    weight: "2",
+    category: "E",
+  },
+  "3B": {},
+  "2A": {
+    weight: "2",
+    category: "S",
+  },
+  "2B": {
+    weight: "2",
+    category: "N",
+  },
+  "4B": {
+    weight: "2",
+    category: "T",
+  },
+  "4A": {
+    weight: "1",
+    category: "F",
+  },
+  "1A": {
+    weight: "2",
+    category: "J",
+  },
+  "1B": {
+    weight: "2",
+    category: "P",
+  },
+  "6A": {
+    weight: "2",
+    category: "E",
+  },
+  "6B": {
+    weight: "1",
+    category: "I",
+  },
+  "5B": {
+    weight: "1",
+    category: "S",
+  },
+  "5A": {
+    weight: "1",
+    category: "N",
+  },
+  "14B": {
+    weight: "2",
+    category: "T",
+  },
+  "14A": {
+    weight: "1",
+    category: "F",
+  },
+  "7A": {
+    weight: "1",
+    category: "J",
+  },
+  "7B": {
+    weight: "1",
+    category: "P",
+  },
+  "9A": {
+    weight: "2",
+    category: "E",
+  },
+  "9B": {
+    weight: "1",
+    category: "I",
+  },
+  "10A": {
+    weight: "1",
+    category: "S",
+  },
+  "10B": {
+    weight: "2",
+    category: "N",
+  },
+  "22B": {
+    weight: "2",
+    category: "T",
+  },
+  "22A": {
+    weight: "2",
+    category: "F",
+  },
+  ZZ: {
+    weight: "0",
+    category: "I",
+  },
+  "7C": {
+    weight: "1",
+    category: "P",
+  },
+  "13A": {
+    weight: "1",
+    category: "E",
+  },
+  "13B": {
+    weight: "2",
+    category: "I",
+  },
+  "12A": {
+    weight: "1",
+    category: "S",
+  },
+  "12B": {
+    weight: "2",
+    category: "N",
+  },
+  "30A": {
+    weight: "2",
+    category: "T",
+  },
+  "30B": {
+    weight: "1",
+    category: "F",
+  },
+  "8A": {
+    weight: "1",
+    category: "J",
+  },
+  "8B": {
+    weight: "2",
+    category: "P",
+  },
+  "16A": {
+    weight: "2",
+    category: "E",
+  },
+  "16B": {
+    weight: "2",
+    category: "I",
+  },
+  "15B": {
+    weight: "1",
+    category: "S",
+  },
+  "15A": {
+    weight: "0",
+    category: "N",
+  },
+  "32A": {
+    weight: "1",
+    category: "T",
+  },
+  "32B": {
+    weight: "1",
+    category: "F",
+  },
+  "11A": {
+    weight: "2",
+    category: "J",
+  },
+  "11B": {
+    weight: "1",
+    category: "P",
+  },
+  "21A": {
+    weight: "2",
+    category: "E",
+  },
+  "21B": {
+    weight: "2",
+    category: "I",
+  },
+  "20A": {
+    weight: "2",
+    category: "S",
+  },
+  "20B": {
+    weight: "2",
+    category: "N",
+  },
+  "33B": {
+    weight: "2",
+    category: "T",
+  },
+  "33A": {
+    weight: "0",
+    category: "F",
+  },
+  "17A": {
+    weight: "2",
+    category: "J",
+  },
+  "17B": {
+    weight: "2",
+    category: "P",
+  },
+  "24A": {
+    weight: "1",
+    category: "E",
+  },
+  "24B": {
+    weight: "1",
+    category: "I",
+  },
+  "23B": {
+    weight: "2",
+    category: "S",
+  },
+  "23A": {
+    weight: "1",
+    category: "N",
+  },
+  "37A": {
+    weight: "1",
+    category: "T",
+  },
+  "37B": {
+    weight: "2",
+    category: "F",
+  },
+  "18A": {
+    weight: "1",
+    category: "J",
+  },
+  "18B": {
+    weight: "1",
+    category: "P",
+  },
+  "26A": {
+    weight: "1",
+    category: "E",
+  },
+  "26B": {
+    weight: "0",
+    category: "I",
+  },
+  "28A": {
+    weight: "2",
+    category: "S",
+  },
+  "28B": {
+    weight: "1",
+    category: "N",
+  },
+  "39A": {
+    weight: "1",
+    category: "T",
+  },
+  "39B": {
+    weight: "1",
+    category: "F",
+  },
+  "19A": {
+    weight: "1",
+    category: "J",
+  },
+  "19B": {
+    weight: "1",
+    category: "P",
+  },
+  "29B": {
+    weight: "2",
+    category: "E",
+  },
+  "29A": {
+    weight: "2",
+    category: "I",
+  },
+  "31B": {
+    weight: "2",
+    category: "S",
+  },
+  "31A": {
+    weight: "0",
+    category: "N",
+  },
+  "40B": {
+    weight: "2",
+    category: "T",
+  },
+  "40A": {
+    weight: "1",
+    category: "F",
+  },
+  "25A": {
+    weight: "1",
+    category: "J",
+  },
+  "25B": {
+    weight: "1",
+    category: "P",
+  },
+  "36B": {
+    weight: "2",
+    category: "E",
+  },
+  "36A": {
+    weight: "1",
+    category: "I",
+  },
+  "35A": {
+    weight: "2",
+    category: "S",
+  },
+  "35B": {
+    weight: "1",
+    category: "N",
+  },
+  "44A": {
+    weight: "1",
+    category: "T",
+  },
+  "44B": {
+    weight: "2",
+    category: "F",
+  },
+  "25C": {
+    weight: "0",
+    category: "J",
+  },
+  "43B": {
+    weight: "1",
+    category: "E",
+  },
+  "43A": {
+    weight: "1",
+    category: "I",
+  },
+  "38B": {
+    weight: "2",
+    category: "S",
+  },
+  "38A": {
+    weight: "0",
+    category: "N",
+  },
+  "46A": {
+    weight: "2",
+    category: "T",
+  },
+  "46B": {
+    weight: "0",
+    category: "F",
+  },
+  "27A": {
+    weight: "2",
+    category: "J",
+  },
+  "27B": {
+    weight: "2",
+    category: "P",
+  },
+  "42A": {
+    weight: "1",
+    category: "S",
+  },
+  "42B": {
+    weight: "2",
+    category: "N",
+  },
+  "47B": {
+    weight: "2",
+    category: "T",
+  },
+  "47A": {
+    weight: "1",
+    category: "F",
+  },
+  "34A": {
+    weight: "2",
+    category: "J",
+  },
+  "34B": {
+    weight: "2",
+    category: "P",
+  },
+  "45B": {
+    weight: "2",
+    category: "S",
+  },
+  "45A": {
+    weight: "0",
+    category: "N",
+  },
+  "49A": {
+    weight: "2",
+    category: "T",
+  },
+  "49B": {
+    weight: "1",
+    category: "F",
+  },
+  "41A": {
+    weight: "2",
+    category: "J",
+  },
+  "41B": {
+    weight: "2",
+    category: "P",
+  },
+  "48A": {
+    weight: "1",
+    category: "S",
+  },
+  "48B": {
+    weight: "1",
+    category: "N",
+  },
+  "50A": {
+    weight: "2",
+    category: "T",
+  },
+  "50B": {
+    weight: "0",
+    category: "F",
+  },
+};
+
+export default MBTIScoring;
--- frontend/src/components/Forms/RadioGroup.js
+++ frontend/src/components/Forms/RadioGroup.js
@@ -1,37 +1,83 @@
 import React, { Component } from "react";
 import { Form, Radio } from "semantic-ui-react";
 
-// Add props here
+function titleCase(string) {
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
 class RadioGroup extends Component {
-  state = {};
-  handleChange = (e, { value }) => this.setState({ value });
+  state = {
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+    8: "",
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: "",
+    18: "",
+    19: "",
+    20: "",
+    21: "",
+    22: "",
+    23: "",
+    24: "",
+    25: "",
+    26: "",
+  };
+  handleChange = (questionNum, questionAns) => {
+    this.setState(
+      (prev) => ({ [questionNum]: questionAns }),
+      () => console.log(this.state)
+    );
+  };
+
+  renderQuestions = (questions) => {
+    return Object.keys(questions).map((questionNum, i) => {
+      return this.renderQuestion(questions[questionNum], questionNum);
+    });
+  };
+
+  renderQuestion = (questionObj, questionNum) => {
+    const getQuestionText = questionObj.question;
+    const questionTextWithProperCasing = titleCase(getQuestionText) + "...";
+    return (
+      <>
+        <Form.Field key={questionNum}>
+          <p>{questionNum + ". " + questionTextWithProperCasing}</p>
+        </Form.Field>
+        {Object.keys(questionObj).map((key, i) => {
+          if (key !== "question") {
+            return (
+              <>
+                <Form.Field key={i}>
+                  <Radio
+                    label={titleCase(questionObj[key])}
+                    name={questionNum + key}
+                    checked={this.state[questionNum] === key}
+                    onChange={() => this.handleChange(questionNum, key)}
+                  />
+                </Form.Field>
+              </>
+            );
+          }
+        })}
+      </>
+    );
+  };
 
   render() {
-    return (
-      <Form>
-        <Form.Field>
-          Selected value: <b>{this.state.value}</b>
-        </Form.Field>
-        <Form.Field>
-          <Radio
-            label="Choose this"
-            name="radioGroup"
-            value="this"
-            checked={this.state.value === "this"}
-            onChange={this.handleChange}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Radio
-            label="Or that"
-            name="radioGroup"
-            value="that"
-            checked={this.state.value === "that"}
-            onChange={this.handleChange}
-          />
-        </Form.Field>
-      </Form>
-    );
+    const { questions } = this.props;
+    return <Form>{this.renderQuestions(this.props.questions)}</Form>;
   }
 }
 
