b70d6c7ce0be2e08d634e5692b5c70ec7618636b
john-manuel <jmanuel51801@gmail.com>
2021-03-28 00:44:04 -0700

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,68 @@
-import Main from "components/Feed/Main";
-import Login from "components/Auth/Login";
 import Navbar from "components/Navbar";
+import Footer from "components/Footer";
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
 } from "react-router-dom";
-import Profile from "components/User/Profile";
-import Groups from "components/Groups";
-import Notifications from "components/Notifications";
-import Messages from "components/Messages";
-import CharacterForm from "components/Forms/CharacterForm";
-import Footer from "components/Footer";
-import SignUp from "components/Auth/SignUp";
+import FeedPage from "pages/FeedPage";
+import ProfilePage from "pages/ProfilePage";
+import GroupsPage from "pages/GroupsPage";
+import NotificationsPage from "pages/NotificationsPage";
+import MessagesPage from "pages/MessagesPage";
+import CharacterFormPage from "pages/CharacterFormPage";
+import SignUpPage from "pages/SignUpPage";
+import LoginPage from "pages/LoginPage";
+import LogoutPage from "pages/LogoutPage";
+import MBTISuccess from "components/Forms/MBTISuccess";
+import { getToken, setToken, getUser, setUser } from "AuthHelper";
 
-function App() {
-  const loggedIn = true;
-  return (
-    <>
-      <Router>
-        <div
-          style={{
-            display: "flex",
-            minHeight: "100vh",
-            flexDirection: "column",
-          }}
-        >
-          <Navbar />
-          <Switch>
-            {/* Keep most specifc paths at the top */}
-            {/* Routing model:
-                                                -> messages
-                                                -> profile
-                login -> characterForm -> feed -> notifications
-                                                -> groups
-                                                -> settings
-              */}
+class App extends Component {
+  handleLogin = (token, user) => {
+    setToken(token);
+    setUser(user);
+    console.log("user: " + getUser());
+    console.log("token: " + getToken());
+  };
 
-            <Route path="/profile" component={Profile} />
-            <Route path="/notifications" component={Notifications} />
-            <Route path="/groups" component={Groups} />
-            <Route path="/messages" component={Messages} />
-            <Route path="/characterForm" component={CharacterForm} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/feed" component={Main} />
+  render() {
+    return (
+      <>
+        <Router>
+          <div
+            style={{
+              display: "flex",
+              minHeight: "100vh",
+              flexDirection: "column",
+            }}
+          >
+            <Switch>
+              <Route path="/profile" component={ProfilePage} />
+              <Route path="/notifications" component={NotificationsPage} />
+              <Route path="/groups" component={GroupsPage} />
+              <Route path="/messages" component={MessagesPage} />
+              <Route path="/characterForm" component={CharacterFormPage} />
+              <Route path="/signup" component={SignUpPage} />
+              <Route path="/feed" component={FeedPage} />
+              <Route path="/success" component={MBTISuccess} />
+              <Route
+                path="/login"
+                component={() => <LoginPage onLogin={this.handleLogin} />}
+              />
+              <Route path="/logout" component={LogoutPage} />
 
-            <Route path="/" exact>
-              {loggedIn ? <Redirect to="/feed" /> : <Login />}
-            </Route>
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    </>
-  );
+              <Route path="/" exact>
+                {<Redirect to="/feed" />}
+              </Route>
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
